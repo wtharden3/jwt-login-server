0fabd8f9f8a4540550b936eda1c86a5c22730f13
Whitney Harden <wtharden320@gmail.com>
2022-03-22 20:58:57 -0400

    set up db

--- database.sql
+++ database.sql
@@ -1,2 +1,13 @@
-CREATE DATABASE jwtloginserverdb;
+CREATE DATABASE jwtlogindb;
 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users(
+  user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_name VARCHAR(255) NOT NULL,
+  user_email VARCHAR(255) NOT NULL,
+  user_password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO users(user_name, user_email, user_password)
+VALUES ('whit1', 'whit1@email.com', 'whit1password');
